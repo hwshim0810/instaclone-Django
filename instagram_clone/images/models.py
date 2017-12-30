1c606f2f1ec0b7f91f2eb483e3afd1920e3d395f
hyunwoo.shim <shim.hw0810@gmail.com>
2017-12-30 17:07:22 +0900

    Create feed api

--- instagram_clone/images/models.py
+++ instagram_clone/images/models.py
@@ -22,10 +22,13 @@ class Image(TimeStampedModel):
     file = models.ImageField()
     location = models.CharField(max_length=140)
     caption = models.TextField()
-    creator = models.ForeignKey(user_models.User, null=True)
+    creator = models.ForeignKey(user_models.User, null=True, related_name='images')
 
     def __str__(self):
         return '{} - {}'.format(self.location, self.caption)
+
+    class Meta:
+        ordering = ['-created_at']
 
 
 @python_2_unicode_compatible
@@ -35,7 +38,7 @@ class Comment(TimeStampedModel):
 
     message = models.TextField()
     creator = models.ForeignKey(user_models.User, null=True)
-    image = models.ForeignKey(Image, null=True)
+    image = models.ForeignKey(Image, null=True, related_name='comments')
 
     def __str__(self):
         return self.message
@@ -47,7 +50,7 @@ class Like(TimeStampedModel):
     """ Like Model """
 
     creator = models.ForeignKey(user_models.User, null=True)
-    image = models.ForeignKey(Image, null=True)
+    image = models.ForeignKey(Image, null=True, related_name='likes')
 
     def __str__(self):
         return 'User: {} - Image Caption: {}'.format(self.creator.username, self.image.caption)
