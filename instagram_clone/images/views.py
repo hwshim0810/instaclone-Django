c88066494013669d3da0fe55c65188c943a8ffd1
hyunwoo.shim <shim.hw0810@gmail.com>
2018-01-01 15:27:41 +0900

    Update like view status code

--- instagram_clone/images/views.py
+++ instagram_clone/images/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from . import models, serializers
 
@@ -38,7 +39,7 @@ class LikeImage(APIView):
         try:
             found_image = models.Image.objects.get(id=image_id)
         except models.Image.DoesNotExist:
-            return Response(status=404)
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
         try:
             preexisting_like = models.Like.objects.get(
@@ -46,12 +47,12 @@ class LikeImage(APIView):
                 image=found_image
             )
             preexisting_like.delete()
-            return Response(status=200)
+            return Response(status=status.HTTP_204_NO_CONTENT)
 
         except models.Image.DoesNotExist:
             new_like = models.Like.objects.create(
                creator=user,
                 image=found_image
             )
-            return Response(status=200)
+            return Response(status=status.HTTP_201_CREATED)
         
