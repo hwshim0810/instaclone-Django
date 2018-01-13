46ae2312c7b5c796130440fbdcc41ee29b365551
hyunwoo.shim <shim.hw0810@gmail.com>
2018-01-13 14:01:09 +0900

    Add get liked user api

--- instagram_clone/images/views.py
+++ instagram_clone/images/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from instagram_clone.notifications import views as notification_views
+from instagram_clone.users import models as user_models
+from instagram_clone.users import serializers as user_serializers
 
 from . import models, serializers
 
@@ -39,6 +41,18 @@ class Feed(APIView):
 
 
 class LikeImage(APIView):
+
+    def get(self, request, image_id, format=None):
+
+        likes = models.Like.objects.filter(image__id=image_id)
+
+        like_creators_ids = likes.values('creator_id')
+
+        users = user_models.User.objects.filter(id__in=like_creators_ids)
+
+        serializer = user_serializers.ListUserSerializer(users, many=True)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request, image_id, format=None):
 
