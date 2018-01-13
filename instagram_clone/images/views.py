912dde5762a5a11543f91420b59684104129d6a1
hyunwoo.shim <shim.hw0810@gmail.com>
2018-01-13 14:23:54 +0900

    Add delete image api

--- instagram_clone/images/views.py
+++ instagram_clone/images/views.py
@@ -185,6 +185,15 @@ class ModerateComments(APIView):
 
 class ImageDetail(APIView):
 
+    def find_own_image(self, image_id, user):
+
+        try:
+            image = models.Image.objects.get(id=image_id, creator=user)
+        except models.Image.DoesNotExist:
+            return None
+        
+        return image
+
     def get(self, request, image_id, format=None):
 
         user = request.user
@@ -202,9 +211,9 @@ class ImageDetail(APIView):
 
         user = request.user
 
-        try:
-            image = models.Image.objects.get(id=image_id, creator=user)
-        except models.Image.DoesNotExist:
+        image = self.find_own_image(image_id, user)
+
+        if image is None:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
 
         serializer = serializers.InputImageSerializer(image, data=request.data, partial=True)  # Partial Update : Not require all fields
@@ -218,3 +227,17 @@ class ImageDetail(APIView):
         else:
 
             return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def delete(self, request, image_id, format=None):
+
+        user = request.user
+
+        image = self.find_own_image(image_id, user)
+
+        if image is None:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+        image.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
