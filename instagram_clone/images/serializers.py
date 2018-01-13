debcd1e27b5ec8db742ea933f4deb570dbc323f7
hyunwoo.shim <shim.hw0810@gmail.com>
2018-01-13 14:13:08 +0900

    Add update image api

--- instagram_clone/images/serializers.py
+++ instagram_clone/images/serializers.py
@@ -75,3 +75,13 @@ class ImageSerializer(serializers.ModelSerializer):
             'created_at'
         )
 
+
+class InputImageSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.Image
+        fields = (
+            'file',
+            'location',
+            'caption',
+        )
