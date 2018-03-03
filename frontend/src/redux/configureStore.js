43fc45c23c8cd5d12e04143ad72536d159cc358a
hyunwoo.shim <shim.hw0810@gmail.com>
2018-03-03 19:05:20 +0900

    Add redux-logger

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -3,8 +3,17 @@ import thunk from 'redux-thunk';
 import users from 'redux/modules/users';
 
 
+const env = process.env.NODE_ENV;
+
 // Apply all env
 const middlewares = [thunk];
+
+// Apply dev env
+if (env === 'development') {
+    // Dynamic package loading
+    const { logger } = require('redux-logger');
+    middlewares.push(logger);
+}
 
 const reducer = combineReducers({
     users
