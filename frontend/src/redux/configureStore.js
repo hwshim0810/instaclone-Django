9bb0a7ac78a6bb3afc65ff48ca33f699b45ae498
hyunwoo.shim <shim.hw0810@gmail.com>
2018-03-17 16:55:49 +0900

    Add i18n package

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -3,6 +3,7 @@ import thunk from 'redux-thunk';
 import { routerReducer, routerMiddleware } from 'react-router-redux';
 import createHistory from 'history/createBrowserHistory';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { i18nState } from 'redux-i18n';
 import users from 'redux/modules/users';
 
 
@@ -22,7 +23,8 @@ if (env === 'development') {
 
 const reducer = combineReducers({
     users,
-    routing: routerReducer
+    routing: routerReducer,
+    i18nState
 });
 
 let store;
