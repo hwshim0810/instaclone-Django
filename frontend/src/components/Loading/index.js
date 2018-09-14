974aae323e0ac954071610d2f1d0b31f205ca847
hyunwoo.shim <shim.hw0810@gmail.com>
2018-09-14 13:50:20 +0900

    Create loaing component

---
+++ frontend/src/components/Loading/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './styles.scss';
+
+
+const Loading = props => (
+    <div className={styles.container}>
+        <img
+            className={styles.spinner} 
+            src={require('images/loading.png')}
+            alt='loading' />
+    </div>
+);
+
+export default Loading;
