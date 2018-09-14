a999eb6df20dd19b6b1062bca679e89598b29145
hyunwoo.shim <shim.hw0810@gmail.com>
2018-09-14 15:36:53 +0900

    Create logout action

--- frontend/src/redux/modules/user.js
+++ frontend/src/redux/modules/user.js
@@ -3,6 +3,7 @@
 // actions
 
 const SAVE_TOKEN = "SAVE_TOKEN";
+const LOGOUT = "LOGOUT";
 
 // action creators
 
@@ -11,6 +12,12 @@ function saveToken(token) {
         type: SAVE_TOKEN,
         token
     }
+}
+
+function logout() {
+    return {
+        type: LOGOUT
+    };
 }
 
 // API actions
@@ -97,6 +104,8 @@ function reducer(state = initialState, action) {
     switch(action.type) {
         case SAVE_TOKEN:
             return applySetToken(state, action)
+        case LOGOUT:
+            return applyLogout(state, action)
         default:
             return state;
     }
@@ -116,11 +125,19 @@ function applySetToken(state, action) {
     }
 }
 
+function applyLogout(state, action) {
+    localStorage.removeItem("jwt");
+    return {
+        isLoggedIn: false
+    };
+}
+
 // exports
 const actionCreators = {
     facebookLogin,
     usernameLogin,
-    createAccount
+    createAccount,
+    logout,
 }
 
 export { actionCreators };
