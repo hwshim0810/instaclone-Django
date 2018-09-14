173a416098db19f04132d27e2f8d37e906d680be
hyunwoo.shim <shim.hw0810@gmail.com>
2018-09-15 00:59:47 +0900

    Update feed component with feedphoto component

--- frontend/src/components/FeedPhoto/presenter.js
+++ frontend/src/components/FeedPhoto/presenter.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './styles.scss';
+import PhotoActions from 'components/PhotoActions';
 
 
 const FeedPhoto = (props, context) => {
-    return <div className={styles.feedPhoto}>FP</div>
+    return (
+        <div className={styles.feedPhoto}>
+            <header className={styles.header}>
+                <img
+                    src={props.creator.profile_image || require('images/noPhoto.jpg')}
+                    alt={props.creator.username} 
+                />
+                <div className={styles.headerColumn}>
+                    <span className={styles.creator}>{props.creator.username}</span>
+                    <span className={styles.location}>{props.location}</span>
+                </div>
+            </header>
+            <img
+                src={props.file}
+                alt={props.caption}
+            />
+            <div className={styles.meta}>
+                <PhotoActions number={props.like_count} />
+            </div>
+        </div>
+    );
 };
 
 FeedPhoto.propTypes = {
