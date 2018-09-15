8a2c6c520eb9cc628a326130342625ea23c4a2d6
hyunwoo.shim <shim.hw0810@gmail.com>
2018-09-15 22:14:05 +0900

    Create commentbox component

---
+++ frontend/src/components/CommentBox/container.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import CommentBox from './presenter';
+
+
+class Container extends Component {
+
+    render() {
+        return <CommentBox />;
+    }
+
+}
+
+export default Container;
